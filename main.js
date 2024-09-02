c001e8f5a0b626e77027fce556c1eaa37fae7a93
Vinayak More <vinayak.more0914@gmail.com>
2024-09-03 02:03:43 +0530

    game loop enhancements

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const TILE_W = SW / TILE_COUNT;
 const bgColor = "green";
 const road = "brown";
 const FPS = 60;
+let GOLD = 100;
+let CASTLE_HEALTH = 1000;
+let intervalId;
 
 class Vector {
   constructor(x, y) {
@@ -67,6 +70,7 @@ class Solder{
     this.speed = speed;
     this.accumulatedPath = new Vector(startPos.x, startPos.y);
     this.currentVector = 0;
+    this.initialHealth = health;
     this.health = health;
   }
   updatePosition = () =>{
@@ -82,36 +86,46 @@ class Solder{
         this.accumulatedPath.y += path[this.currentVector].y;
         this.currentVector++;
       }
-
+    if(this.reachedCastle()) CASTLE_HEALTH -= this.health;
+  }
+  reachedCastle = () => {
+    if(this.x >= SW-TILE_W && this.y >= SH - TILE_W) return true;
   }
   render = ()=>{
     this.updatePosition();
     this.context.fillStyle = 'blue';
     this.context.beginPath();
-    this.context.arc(this.x, this.y, TILE_W * 0.8, 0, 2 * Math.PI);
+    this.context.arc(this.x, this.y, TILE_W * 0.8 * (this.health/this.initialHealth), 0, 2 * Math.PI);
     this.context.fill();
   }
 }
 
 let solders = [
-  new Solder(context, TILE_W * startPos.x, TILE_W * startPos.y, 1, 100),
+  new Solder(context, TILE_W * startPos.x, TILE_W * startPos.y, 1, 1000),
   new Solder(context, TILE_W * startPos.x, TILE_W * startPos.y, 2, 200),
+  new Solder(context, TILE_W * startPos.x, TILE_W * startPos.y, 3, 150),
+  new Solder(context, TILE_W * startPos.x, TILE_W * startPos.y, 4, 100),
+  new Solder(context, TILE_W * startPos.x, TILE_W * startPos.y, 5, 1000),
 ];
 
 function renderSolders(){
-  solders = solders.filter(solder => solder.health > 0);
+  solders = solders.filter(solder => solder.health > 0 && !solder.reachedCastle());
   for(let solder of solders){
-    if(solder.health > 0) solder.render();
+    solder.render();
   }
 }
 
 class Tower{
   //x and y are top left corner co-ordinates
   //rate is every rateth frame it will do damage on one closest solder
-  constructor(context, x, y, rate, damage){
+  constructor(context, x, y, rate, damage, cost){
     this.context = context;
-    this.x = this.find(x);
-    this.y = this.find(y);
+    this.x = this.find(x) + TILE_W;
+    this.y = this.find(y) + TILE_W;
+    this.rate = rate;
+    this.epoc = rate;
+    this.damage = damage;
+    this.cost = cost;
     console.log("found", this.x, this.y);
   }
   find = (value) => {
@@ -120,8 +134,29 @@ class Tower{
   render = ()=>{
     this.context.fillStyle = 'orange';
     this.context.beginPath();
-    this.context.arc(this.x + TILE_W, this.y + TILE_W , TILE_W * 0.8, 0, 2 * Math.PI);
+    this.context.arc(this.x, this.y, TILE_W * 0.8, 0, 2 * Math.PI);
     this.context.fill();
+    this.attack();
+  }
+  attack = () => {
+    let range = TILE_W * 3;
+    if(this.epoc == this.rate){
+      let solderToAttack = solders.find(solder => 
+        solder.x >= (this.x - range) && 
+        solder.x <= (this.x + range) &&
+        solder.y >= (this.y - range) && 
+        solder.y <= (this.y + range)
+      )
+      if(solderToAttack){
+        solderToAttack.health -= this.damage;
+        drawLine(this.context, this.x, this.y, solderToAttack.x, solderToAttack.y, 'yellow');
+        this.epoc--;
+        if(solderToAttack.health <= 0) GOLD += solderToAttack.initialHealth;
+      }
+    }
+    if(this.epoc < this.rate)
+      this.epoc--;
+    if(this.epoc <= 0) this.epoc = this.rate;
   }
 }
 const towers = [];
@@ -134,17 +169,31 @@ function renderTowers(){
 
 function addTower(x, y){
   console.log(pathCoord);
-  const tower = new Tower(context, x, y, 10, 5)
-  if(validCellForTower(tower))
+  const tower = new Tower(context, x, y, 10, 5, 10)
+  if(validCellForTower(tower) && hasGold(tower)){
+    GOLD -= tower.cost;
+    console.log("gold left", GOLD);
     towers.push(tower);
+  }
 }
 
 function validCellForTower(tower){
   return pathCoord[tower.y/TILE_W][tower.x / TILE_W] != 1;
 }
 
+function hasGold(tower){
+  return GOLD >= tower.cost;
+}
 function updateState() {
-  
+  document.getElementById("castle_health").innerText = CASTLE_HEALTH;
+  document.getElementById("gold").innerText = GOLD;
+  if(CASTLE_HEALTH < 0 || solders.length == 0){
+    let result = "";
+    if(CASTLE_HEALTH > 0) result = "You WON!!"
+    else result = "You have lost!!" 
+    document.getElementById("result").innerText = result;
+    clearInterval(intervalId);
+  }
 }
 
 function renderGrid() {
@@ -155,7 +204,8 @@ function renderGrid() {
     drawLine(context, 0, i * TILE_W, SW, i * TILE_W);
   }
 }
-function drawLine(context, x1, y1, x2, y2) {
+function drawLine(context, x1, y1, x2, y2, style='black') {
+  context.strokeStyle = style;
   context.beginPath();
   context.moveTo(x1, y1);
   context.lineTo(x2, y2);
@@ -177,7 +227,7 @@ function play() {
   render();
 }
 
-setInterval(() => play(), 1000 / FPS);
+intervalId = setInterval(() => play(), 1000 / FPS);
 //play();
 
 canvas.addEventListener("click", (event)=>{
